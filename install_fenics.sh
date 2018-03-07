dc4fc8f3c97c0e26d1fef5495fb77d4b997d6ea0
Fabian Loeschner <fabian.loeschner@rwth-aachen.de>
2018-03-07 19:11:59 +0100

    Added sources of TSFC

--- install_fenics.sh
+++ install_fenics.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir fenics
-cd fenics
+mkdir ~/fenics
+cd ~/fenics
 
 virtualenv fenics_env
 . ./fenics_env/bin/activate
@@ -9,15 +9,24 @@ virtualenv fenics_env
 mkdir src
 cd src
 
-git clone git@bitbucket.org:fenics-project/fiat
-git clone git@bitbucket.org:fenics-project/ufl
-git clone git@bitbucket.org:fenics-project/dijitso
-git clone git@bitbucket.org:fenics-project/ffc
+git clone https://bitbucket.org/fenics-project/fiat.git
+git clone https://bitbucket.org/fenics-project/ufl.git
+git clone https://bitbucket.org/fenics-project/dijitso.git
+git clone https://bitbucket.org/fenics-project/ffc.git
+
+git clone https://github.com/blechta/tsfc.git
+git clone https://github.com/blechta/COFFEE.git
+git clone https://github.com/blechta/FInAT.git
 
 cd fiat && pip3 install -e . && cd ..
 cd ufl && pip3 install -e . && cd ..
 cd dijitso && pip3 install -e . && cd ..
 cd ffc && pip3 install -e . && cd ..
+
+cd tsfc && pip3 install -e . && cd ..
+cd COFFEE && pip3 install -e . && cd ..
+cd FInAT && pip3 install -e . && cd ..
+
 cd ..
 
 echo
