ac5ef8b82f6144db1e3de4df74c335bb0f90deb2
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2018-03-11 18:53:46 +0100

    Implemented repository cloning to python install script

--- install_fenics.py
+++ install_fenics.py
@@ -1,22 +1,31 @@
 import os
 import argparse
 import virtualenv
+import subprocess
 
-def clone_repos():
-	"""
-	cd "${SRC_DIR}"
-	git clone https://bitbucket.org/fenics-project/fiat.git
-	git clone https://bitbucket.org/fenics-project/ufl.git
-	git clone https://bitbucket.org/fenics-project/dijitso.git
-	git clone https://bitbucket.org/fenics-project/ffc.git
+def clone_repos(src_directory: str):
+	repos = [
+		"https://bitbucket.org/fenics-project/fiat.git",
+		"https://bitbucket.org/fenics-project/ufl.git",
+		"https://bitbucket.org/fenics-project/dijitso.git",
+		"https://bitbucket.org/fenics-project/ffc.git",
+		"https://github.com/blechta/tsfc.git",
+		"https://github.com/blechta/COFFEE.git",
+		"https://github.com/blechta/FInAT.git",
+		"https://bitbucket.org/fenics-project/dolfin.git",
+		"https://github.com/pybind/pybind11.git"
+		]
 
-	git clone https://github.com/blechta/tsfc.git
-	git clone https://github.com/blechta/COFFEE.git
-	git clone https://github.com/blechta/FInAT.git
+	for repo in repos:
+		process = subprocess.Popen(["git", "clone", repo], stdout=subprocess.PIPE, cwd=src_directory)
+		while True:
+			output = process.stdout.readline()
+			if len(output.strip()) == 0 and process.poll() is not None:
+				print()
+				break
+			if output:
+				print(str(output.strip()))
 
-	git clone https://bitbucket.org/fenics-project/dolfin.git
-	git clone https://github.com/pybind/pybind11.git
-	"""
 	pass
 
 def pip_install():
@@ -100,35 +109,42 @@ if SRC_DIR is None:
 	SRC_DIR = os.path.join(FENICS_DIR, "src")
 
 print(args)
-print(FENICS_DIR)
-print(SRC_DIR)
+print()
 
 VENV_DIR = os.path.join(FENICS_DIR, "fenics_env")
 BUILD_DIR = os.path.join(FENICS_DIR, "build")
 PYBIND_DIR = os.path.join(FENICS_DIR, "include", "pybind11")
 DOLFIN_DIR = os.path.join(FENICS_DIR, "dolfin")
 
-if not os.path.exists(FENICS_DIR):
-    os.makedirs(FENICS_DIR)
+if args.clone_only is True:
+	print("Only cloning repos...")
+	print()
 
-if not os.path.exists(SRC_DIR):
-    os.makedirs(SRC_DIR)
+	if not os.path.exists(SRC_DIR):
+		os.makedirs(SRC_DIR)
 
+	clone_repos(SRC_DIR)
+else:
+	if not os.path.exists(FENICS_DIR):
+		os.makedirs(FENICS_DIR)
 
-# Create and activate the virtual environment
-virtualenv.create_environment(VENV_DIR)
-exec(open(os.path.join(VENV_DIR, "bin", "activate_this.py")).read())
+	if not os.path.exists(SRC_DIR):
+		os.makedirs(SRC_DIR)
 
-clone_repos()
-pip_install()
+	# Create and activate the virtual environment
+	virtualenv.create_environment(VENV_DIR)
+	exec(open(os.path.join(VENV_DIR, "bin", "activate_this.py")).read())
 
-pybind_build()
-dolfin_build()
+	clone_repos(SRC_DIR)
+	pip_install()
 
-#print(pip.main(["list"]))
+	pybind_build()
+	dolfin_build()
 
-#pip3 list
+	#print(pip.main(["list"]))
 
-print("")
-print(f"Activate FEniCS python virtualenv using 'source {FENICS_DIR}/fenics_env/bin/activate'")
-print(f"Activate DOLFIN build environment using 'source ${FENICS_DIR}/dolfin/share/dolfin/dolfin.conf'")
+	#pip3 list
+
+	print("")
+	print(f"Activate FEniCS python virtualenv using 'source {FENICS_DIR}/fenics_env/bin/activate'")
+	print(f"Activate DOLFIN build environment using 'source ${FENICS_DIR}/dolfin/share/dolfin/dolfin.conf'")
