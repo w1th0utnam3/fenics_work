7a3753823c5cbe3f07aa23591f83c32a96084cf6
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2018-06-28 18:40:22 +0200

    Add new cross element FFC flags to benchmark

--- main/simd/benchmarker/benchmarker/main.py
+++ main/simd/benchmarker/benchmarker/main.py
@@ -65,6 +65,18 @@ def gen_test_case() -> TestCase:
         ffc_args=ffc_default
     )
 
+    four_elements_1 = TestRunArgs(
+        name="4x ce, scalar sp",
+        cross_element_width=4,
+        ffc_args={"optimize": True, "enable_cross_element_array_conv": True}
+    )
+
+    four_elements_2 = TestRunArgs(
+        name="4x ce, ssp, fused",
+        cross_element_width=4,
+        ffc_args={"optimize": True, "enable_cross_element_array_conv": True, "enable_cross_element_fuse": True}
+    )
+
     # Combine all options: benchmark will be cartesian product of compiler_args, run_args and forms
     test_case = TestCase(
         compiler_args=[
@@ -74,7 +86,9 @@ def gen_test_case() -> TestCase:
         run_args=[
             one_element,
             one_element_padded,
-            four_elements
+            four_elements,
+            four_elements_1,
+            four_elements_2
         ],
         forms=forms.get_all_forms(),
         reference_case=(0, 0),
