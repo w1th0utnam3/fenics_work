1598f2bce32e16617d06d66aa93888faae3d157a
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2018-07-22 11:16:24 +0200

    Updated cross assembly test

--- main/simd/cross_assembly.py
+++ main/simd/cross_assembly.py
@@ -1,7 +1,7 @@
 import dolfin
 import dolfin.cpp
 from dolfin import *
-from dolfin.la import PETScMatrix, PETScVector
+from dolfin.la import PETScMatrix, PETScVector, PETScKrylovSolver
 from dolfin.jit.jit import ffc_jit
 
 import numpy as np
@@ -27,6 +27,8 @@ bc = DirichletBC(Q, u0, boundary)
 a = dolfin.cpp.fem.Form([Q._cpp_object, Q._cpp_object])
 L = dolfin.cpp.fem.Form([Q._cpp_object])
 
+print("Form compilation...")
+
 # Bilinear form
 jit_result = ffc_jit(dot(grad(u), grad(v)) * dx,
                      form_compiler_parameters={"cell_batch_size": 4, "enable_cross_element_gcc_ext": True})
@@ -47,12 +49,13 @@ assembler = dolfin.cpp.fem.Assembler([[a]], [L], [bc])
 A = PETScMatrix()
 b = PETScVector()
 
+print("Running assembly...")
 assembler.assemble(A, dolfin.cpp.fem.Assembler.BlockType.monolithic)
 assembler.assemble(b, dolfin.cpp.fem.Assembler.BlockType.monolithic)
 
 Anorm = A.norm(dolfin.cpp.la.Norm.frobenius)
 bnorm = b.norm(dolfin.cpp.la.Norm.l2)
-print(Anorm, bnorm)
+print("A.norm(frobenius)={:.12e}\nb.norm(l2)={:.12e}".format(Anorm, bnorm))
 
 # Norms obtained with n=22 and bcs
 assert (np.isclose(Anorm, 60.86192203436385))
@@ -62,11 +65,15 @@ assert (np.isclose(bnorm, 0.018075523965828778))
 #assert (np.isclose(Anorm, 29.416127208482518))
 #assert (np.isclose(bnorm, 0.018726593629987284))
 
-"""
+print("Running solver...")
 comm = L.mesh().mpi_comm()
 solver = PETScKrylovSolver(comm)
 
 u = Function(Q)
 solver.set_operator(A)
 solver.solve(u.vector(), b)
-"""
+
+unorm = u.vector().norm(dolfin.cpp.la.Norm.l2)
+print("u.norm(l2)={:.12e}".format(unorm))
+
+assert (np.isclose(unorm, 5.1387821818667))
