979d3f22dea1fe029163ba440e893c73dbca3000
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2018-07-30 13:09:33 +0200

    Updated issue code

--- simd/issue.py
+++ simd/issue.py
@@ -1,5 +1,5 @@
 import ufl
-from ufl import inner, dx
+from ufl import inner, curl, dx
 
 from minidolfin.assembling import jit_compile_form
 
@@ -7,12 +7,12 @@ from minidolfin.assembling import jit_compile_form
 # for fixing https://github.com/FEniCS/ffcx/pull/25
 
 cell = ufl.tetrahedron
-element = ufl.VectorElement("Nedelec 1st kind H(curl)", cell, 3)
+element = ufl.FiniteElement("Nedelec 1st kind H(curl)", cell, 3)
 
 u = ufl.TrialFunction(element)
 v = ufl.TestFunction(element)
 
-a = inner(u, v)*dx
+a = inner(curl(u), curl(v))*dx - inner(u, v)*dx
 
 jit_compile_form(a, {"compiler": "ffc",
                      "max_preintegrated_unrolled_table_size": 2000})
